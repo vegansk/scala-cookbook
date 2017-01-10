54ae1be1670c073819860bc891b966fefa96064c
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2017-01-10 18:02:49 +0700

    Rewrite project

--- build.sbt
+++ build.sbt
@@ -1,63 +1,17 @@
+import Cookbook._
+
 scalaVersion in ThisBuild := Versions.scala
 
-def mkProject(name: String, path: File): Project = (
-  Project(name, path)
-  settings(Settings.common)
-)
+// Projects
 
-lazy val typesOfTypes = mkProject("typesOfTypes", file("types_of_types"))
+val root = Projects.root
+val typesOfTypes = Projects.typesOfTypes
+val examplesMacros = Projects.examplesMacros
+val examplesCore = Projects.examplesCore
 
-lazy val examplesMacros = mkProject("examplesMacros", file("examples") / "macros")
-  .settings(
-  libraryDependencies += Dependencies.scalaMeta
-)
-  .settings(Settings.macros)
+// Consoles
 
-lazy val examples = mkProject("examples", file("examples") / "core")
-  .settings(
-  libraryDependencies += Dependencies.scalaMeta
-)
-  .settings(Settings.macros)
-  .dependsOn(examplesMacros)
-
-lazy val conScalaz = mkProject("conScalaz", file("consoles") / "scalaz")
-  .settings(
-  libraryDependencies += Dependencies.scalaz,
-  initialCommands in console := """
-import scalaz._
-import Scalaz._
-"""
-)
-
-lazy val conScalaMeta = mkProject("conScalaMeta", file("consoles") / "scalameta")
-  .settings(
-  libraryDependencies += Dependencies.scalaMeta,
-  libraryDependencies += Dependencies.pprint,
-  initialCommands in console := """
-import scala.meta._
-import pprint._
-"""
-)
-
-lazy val conMonocle = mkProject("conMonocle", file("consoles") / "monocle")
-  .settings(
-  libraryDependencies ++= Dependencies.monocle,
-  addCompilerPlugin("org.scalamacros" %% "paradise" % "2.1.0" cross CrossVersion.full),
-  initialCommands in console := """
-import monocle._
-import monocle.macros._
-"""
-)
-
-lazy val conMacros = mkProject("conMacros", file("consoles") / "macros")
-  .settings(
-  scalacOptions += "-Yreify-copypaste",
-  libraryDependencies += Dependencies.scalaReflect,
-  libraryDependencies += Dependencies.pprint,
-  initialCommands in console := """
-import scala.language.experimental.macros
-import scala.reflect.runtime.universe
-import universe._
-import pprint._
-"""
-)
+val conScalaz = Consoles.conScalaz
+val conScalaMeta = Consoles.conScalaMeta
+val conMonocle = Consoles.conMonocle
+val conMacros = Consoles.conMacros
---
+++ project/Build.scala
@@ -0,0 +1,111 @@
+import sbt._
+import Keys._
+
+object Cookbook {
+
+  object Versions {
+    val scala = "2.11.8"
+    val scalaz = "7.2.7"
+    val scalaMeta = "1.3.0"
+    val monocle = "1.3.2"
+  }
+
+  object Dependencies {
+    lazy val scalaz = "org.scalaz" %% "scalaz-core" % Versions.scalaz
+    lazy val scalaMeta = "org.scalameta" %% "scalameta" % Versions.scalaMeta
+    lazy val scalaReflect = "org.scala-lang" % "scala-reflect" % Versions.scala
+    lazy val pprint = "com.lihaoyi" %% "pprint" % "0.4.3"
+
+    lazy val monocle = Seq(
+      "com.github.julien-truffaut" %% "monocle-core" % Versions.monocle,
+      "com.github.julien-truffaut" %% "monocle-macro" % Versions.monocle
+    )
+  }
+
+  type PC = Project => Project
+
+  def commonSettings: PC =
+    _.settings(
+      scalaVersion := Versions.scala
+    )
+
+  def macrosSettings: PC =
+    _.settings(
+      resolvers += Resolver.url(
+        "scalameta",
+        url("http://dl.bintray.com/scalameta/maven"))(Resolver.ivyStylePatterns),
+      addCompilerPlugin(
+        "org.scalameta" % "paradise" % "3.0.0.132" cross CrossVersion.full),
+      scalacOptions += "-Xplugin-require:macroparadise",
+      libraryDependencies ++= Seq(
+        Dependencies.scalaReflect,
+        Dependencies.scalaMeta
+      )
+    )
+
+  object Projects {
+
+    lazy val root = project.in(file("."))
+      .aggregate(typesOfTypes, examplesCore)
+
+    lazy val typesOfTypes = project.in(file("types_of_types"))
+      .configure(commonSettings)
+
+    lazy val examplesMacros = project.in(file("examples") / "macros")
+      .configure(commonSettings, macrosSettings)
+
+    lazy val examplesCore = project.in(file("examples") / "core")
+      .configure(commonSettings, macrosSettings)
+      .dependsOn(examplesMacros)
+
+  }
+
+  object Consoles {
+
+    lazy val conScalaz = project.in(file("consoles") / "scalaz")
+      .configure(commonSettings)
+      .settings(
+        libraryDependencies += Dependencies.scalaz,
+        initialCommands in console := """
+          | import scalaz._
+          | import Scalaz._
+        """.stripMargin
+      )
+
+    lazy val conScalaMeta = project.in(file("consoles") / "scalameta")
+      .configure(commonSettings)
+      .settings(
+        libraryDependencies += Dependencies.scalaMeta,
+        libraryDependencies += Dependencies.pprint,
+        initialCommands in console := """
+          | import scala.meta._
+          | import pprint._
+        """.stripMargin
+      )
+
+    lazy val conMonocle = project.in(file("consoles") / "monocle")
+      .configure(commonSettings)
+      .settings(
+        libraryDependencies ++= Dependencies.monocle,
+        addCompilerPlugin("org.scalamacros" %% "paradise" % "2.1.0" cross CrossVersion.full),
+        initialCommands in console := """
+          | import monocle._
+          | import monocle.macros._
+        """.stripMargin
+      )
+
+    lazy val conMacros = project.in(file("consoles") / "macros")
+      .configure(commonSettings)
+      .settings(
+        scalacOptions += "-Yreify-copypaste",
+        libraryDependencies += Dependencies.scalaReflect,
+        libraryDependencies += Dependencies.pprint,
+        initialCommands in console := """
+          | import scala.language.experimental.macros
+          | import scala.reflect.runtime.universe
+          | import universe._
+          | import pprint._
+        """.stripMargin
+      )
+  }
+}
