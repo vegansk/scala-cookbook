bb2a1bfc9ccf34fb7e501daeb8aa90e78a4a5b5a
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2017-01-16 15:00:06 +0700

    Add working redux bindings

--- scalajs/bundler-test/src/main/scala/Main.scala
+++ scalajs/bundler-test/src/main/scala/Main.scala
@@ -1,9 +1,43 @@
+import scala.scalajs.js
+import js.annotation._
 import scala.scalajs.js.JSApp
+import scala.scalajs.js.|
+
+case class State(
+  counter: Int
+)
+
+sealed trait Action;
+
+case object Increase extends Action;
+
+case object Decrease extends Action;
 
 object Main extends JSApp {
 
+  def reducer(s: State, a: Action): State =
+    a match {
+      case Increase =>
+        s.copy(counter = s.counter + 1)
+      case Decrease =>
+        s.copy(counter = s.counter - 1)
+    }
+
   def main(): Unit = {
-    println("Hello, world!")
+    println("Create the store...")
+
+    val store = Redux.createStore[State, Action](reducer _, State(0))
+
+    println(s"Initial state is ${store.getState()}")
+
+    store.dispatch(Redux.createAction(Increase))
+
+    println(s"The state after increase action is ${store.getState()}")
+
+    store.dispatch(Redux.createAction(Decrease))
+    store.dispatch(Redux.createAction(Decrease))
+
+    println(s"The state after two decrease actions is ${store.getState()}")
   }
 
 }
--- scalajs/bundler-test/src/main/scala/Redux.scala
+++ scalajs/bundler-test/src/main/scala/Redux.scala
@@ -1,10 +1,8 @@
 import scala.scalajs.js
-import scala.scalajs.js.JSApp
-import scala.scalajs.js.annotation.JSImport
+import js.|
+import js.annotation._
 
-@JSImport("redux", JSImport.Namespace)
-@js.native
-object Redux extends js.Object {
+object Redux {
 
   type Reducer[S, A] = js.Function2[S, A, S]
 
@@ -20,16 +18,58 @@ object Redux extends js.Object {
 
   type Middleware[S, A] = js.Function1[MiddlewareArg[S, A], js.Function1[Dispatcher[A], Dispatcher[A]]]
 
+  type Listener = js.Function0[Unit]
+
+  type Unsubscriber = js.Function0[Unit]
+
   @js.native
   trait Store[S, A] extends js.Object {
+    val getState: StateGetter[S] = js.native
+    val dispatch: Dispatcher[A | js.Object] = js.native
+    val subscribe: js.Function1[Listener, Unsubscriber] = js.native
+    val replaceReducer: js.Function1[Reducer[S, A], Unit] = js.native
   }
 
   type Enhancer[S, A] = js.Function1[Store[S, A], Store[S, A]]
 
+  @JSImport("redux", JSImport.Namespace)
+  @js.native
+  private object Impl extends js.Object {
+    def createStore[S, A](
+      reducer: Reducer[S, A | js.Object],
+      initialState: js.UndefOr[S] = js.undefined,
+      enhancer: js.UndefOr[Enhancer[S, A | js.Object]] = js.undefined
+    ): Store[S, A] = js.native
+  }
+
+  @js.native
+  trait WrappedAction extends js.Object {
+    val `type`: String = js.native
+    val scalaJsReduxAction: js.Any = js.native
+  }
+
+  object WrappedAction {
+    def apply[A](a: A) = js.Dynamic.literal(
+      `type` = "scalaJsReduxAction",
+      scalaJsReduxAction = a.asInstanceOf[js.Any]
+    ).asInstanceOf[WrappedAction]
+  }
+
+  def wrapReducer[S, A](r: Reducer[S, A]): Reducer[S, A | js.Object] =
+    (s: S, a: A | js.Object) => {
+      val aDyn = a.asInstanceOf[js.Dynamic]
+      if (aDyn.scalaJsReduxAction != js.undefined)
+        r(s, aDyn.scalaJsReduxAction.asInstanceOf[A])
+      else
+        s
+    }
+
   def createStore[S, A](
     reducer: Reducer[S, A],
-    initialState: js.UndefOr[S],
-    enhancer: js.UndefOr[Enhancer[S, A]]
-  ): Store[S, A] = js.native
+    initialState: js.UndefOr[S] = js.undefined,
+    enhancer: js.UndefOr[Enhancer[S, A | js.Object]] = js.undefined
+  ): Store[S, A] = Impl.createStore(wrapReducer(reducer), initialState, enhancer)
+
+  def createAction[A](a: A): WrappedAction = WrappedAction(a)
 
 }
